134e55a8b3b87a8f043429e38d640ba213eb3104
Dssolved <lovelyfromhell@gmail.com>
2025-10-02 09:52:54 +0500

    autowired

--- src/main/java/com/example/springdemo/controller/HomeController.java
+++ src/main/java/com/example/springdemo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.springdemo.controller;
 
 import com.example.springdemo.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+	@Autowired
 	private final ItemService itemService;
 
 	@GetMapping
